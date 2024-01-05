9a0cc79c4fe71520a1abdfe69f6a3408613df781
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-05 23:45:03 +0300

    add locatros and methods for searc input

--- src/main/java/by/itacademy/moskalew/pages/ReebokSearchPage.java
+++ src/main/java/by/itacademy/moskalew/pages/ReebokSearchPage.java
@@ -1,4 +1,23 @@
 package by.itacademy.moskalew.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ReebokSearchPage {
+    private String inputSearch = "//input[@class='textbox-input--jX6cr  textbox-input-toggle-off--On9N3' and @placeholder='Search']";
+    private String buttonSearch = "//*[name()='svg' and @class='icon--RZbKK icon-link--seVu2  ']/*[name()='path' and @fill='currentColor']";
+    private WebDriver driver;
+
+    public ReebokSearchPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickButtonSearch() {
+        driver.findElement(By.xpath(buttonSearch)).click();
+    }
+
+    public void sendKeysInputSearch(String search) {
+        driver.findElement(By.xpath(inputSearch)).sendKeys(search);
+    }
+
 }
