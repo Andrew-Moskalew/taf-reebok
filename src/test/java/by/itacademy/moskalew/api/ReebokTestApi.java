f43c55f7ddc4ea42d17e1dc05b16731c511a427d
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-10 21:55:03 +0300

    refactor import

--- src/test/java/by/itacademy/moskalew/api/ReebokTestApi.java
+++ src/test/java/by/itacademy/moskalew/api/ReebokTestApi.java
@@ -1,7 +1,7 @@
 package by.itacademy.moskalew.api;
 
-import org.junit.Test;
 
+import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
