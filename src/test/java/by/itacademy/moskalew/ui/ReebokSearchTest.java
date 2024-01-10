36d58fe1fb3c3db903e163d681b4b8c0e70383f0
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-10 22:23:02 +0300

    refactoring

--- src/test/java/by/itacademy/moskalew/ui/ReebokSearchTest.java
+++ src/test/java/by/itacademy/moskalew/ui/ReebokSearchTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
