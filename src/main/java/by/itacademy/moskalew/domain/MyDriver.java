cf2e0565e64e31454e7f7db20bf65231a2d8898f
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-10 22:06:10 +0300

    add methods getDriver and quitDriver

--- src/main/java/by/itacademy/moskalew/domain/MyDriver.java
+++ src/main/java/by/itacademy/moskalew/domain/MyDriver.java
@@ -1,4 +1,21 @@
 package by.itacademy.moskalew.domain;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MyDriver {
+    private static WebDriver driver;
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
