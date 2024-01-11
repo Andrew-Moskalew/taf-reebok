22dde6719b6490a32dedaea464b2fc8625290220
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-11 23:35:55 +0300

    add locators and methods for waiting

--- src/main/java/by/itacademy/moskalew/pages/ReebokLoginPage.java
+++ src/main/java/by/itacademy/moskalew/pages/ReebokLoginPage.java
@@ -9,10 +9,20 @@ public class ReebokLoginPage {
     private String buttonLogin = "//button[@class='btn--1eE4Z theme-rr-navy--3ttBH size-small--1XFhu checkout-details-reviewbutton--gQTO4']";
     private String inputEmail = "//input[@class='textbox-input--jX6cr' and @name='email']";
     private String inputPassword = "//input[@class='textbox-input--jX6cr' and @name='password']";
-    private String errorMassageEmail = "//p[@class='tag_p--1xo5V theme-error--3VYhx address-form-email--3xtdp']";
-    private String errorMassagePassword = "//p[@class='tag_p--1xo5V theme-error--3VYhx address-form-password--2OqFT']";
-    private String errorMassageLoginForm = "//span[@class='registerForm-LoginError--1mbUB']";
+    private String errorMessageEmail = "//p[@class='tag_p--1xo5V theme-error--3VYhx address-form-email--3xtdp']";
+    private String errorMessagePassword = "//p[@class='tag_p--1xo5V theme-error--3VYhx address-form-password--2OqFT']";
+    private String errorMessageLoginForm = "//span[@class='registerForm-LoginError--1mbUB']";
+    private String headerLogin = "//h1[@class='tag_h1_sm--2PQTc  registerForm-title--2CADy' and text()='Log In']";
+    private String url = "https://www.reebok.com";
     private WebDriver driver;
+
+    public By getHeaderLogin() {
+        return By.xpath(headerLogin);
+    }
+
+    public By getErrorMessageloginForm() {
+        return By.xpath(errorMessageLoginForm);
+    }
 
     public ReebokLoginPage() {
         this.driver = MyDriver.getDriver();
@@ -42,16 +52,20 @@ public class ReebokLoginPage {
         driver.findElement(By.xpath(inputPassword)).sendKeys(password);
     }
 
-    public String getTextErrorMassageEmail() {
-        return driver.findElement(By.xpath(errorMassageEmail)).getText();
+    public String getUrl() {
+        return url;
     }
 
-    public String getTextErrorMassagePassword() {
-        return driver.findElement(By.xpath(errorMassagePassword)).getText();
+    public String getTextErrorMessageEmail() {
+        return driver.findElement(By.xpath(errorMessageEmail)).getText();
     }
 
-    public String getTextErrorMassageLoginForm() {
-        return driver.findElement(By.xpath(errorMassageLoginForm)).getText();
+    public String getTextErrorMessagePassword() {
+        return driver.findElement(By.xpath(errorMessagePassword)).getText();
+    }
+
+    public String getTextErrorMessageLoginForm() {
+        return driver.findElement(By.xpath(errorMessageLoginForm)).getText();
     }
 
 }
--- src/main/java/by/itacademy/moskalew/pages/ReebokSearchPage.java
+++ src/main/java/by/itacademy/moskalew/pages/ReebokSearchPage.java
@@ -9,10 +9,21 @@ import java.util.List;
 
 public class ReebokSearchPage {
     private String inputSearch = "//input[@class='textbox-input--jX6cr  textbox-input-toggle-off--On9N3' and @placeholder='Search']";
-    private String buttonSearch = "//*[name()='svg' and @class='icon--RZbKK icon-link--seVu2  ' and @aria-hidden='true' and @width='24'] ";
+    private String buttonSearch = "//*[name()='svg' and @class='icon--RZbKK icon-link--seVu2  ' and @aria-hidden='true' and @width='24']";
+    private String buttonResetSearch = "//*[name()='svg' and @class='icon--RZbKK icon-link--seVu2  ' and @width='14']";
     private String searchResult = "//h1[@class='tag_h1_w--3KHZV  category-title--rXaZj']";
+    private String headerSearchResult = "//h6[@class='tag_h6_small--1VdSQ' and text()='Your Search results for:']";
     private String searchProductCardName = "//p[@class='tag_p--1xo5V  product-card-name--9ffy7']";
+    private String url = "https://www.reebok.com";
     private WebDriver driver;
+
+    public By getButtonResetSearch() {
+        return By.xpath(buttonResetSearch);
+    }
+
+    public By getHeaderSearchResult() {
+        return By.xpath(headerSearchResult);
+    }
 
     public ReebokSearchPage() {
         this.driver = MyDriver.getDriver();
@@ -24,6 +35,10 @@ public class ReebokSearchPage {
 
     public void sendKeysInputSearch(String search) {
         driver.findElement(By.xpath(inputSearch)).sendKeys(search);
+    }
+
+    public String getUrl() {
+        return url;
     }
 
     public String getTextSearchResult() {
