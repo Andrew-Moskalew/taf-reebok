dbb1f51c237cc71b217b3aaa257177c0e6065c2b
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-12 22:51:07 +0300

    add test methods

--- src/test/java/by/itacademy/moskalew/api/ReebokTestApi.java
+++ src/test/java/by/itacademy/moskalew/api/ReebokTestApi.java
@@ -3,6 +3,7 @@ package by.itacademy.moskalew.api;
 
 import by.itacademy.moskalew.domain.RandomUserData;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -11,13 +12,23 @@ public class ReebokTestApi {
     String url = "https://www.reebok.com/graphql";
 
     @Test
-    public void testLoginWithValidValues() {
-        given().log().all().body(RandomUserData.getBodyWithRandomValidValuesAPI()).
+    public void testLoginValidValues() {
+        given().log().all().body(RandomUserData.getBodyWithValidValues()).
                 header("Content-Type", "application/json")
                 .when().post(url)
                 .then()
                 .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
     }
+
+    @Test
+    public void testLoginInvalidEmail() {
+        given().log().all().body(RandomUserData.getBodyWithInvalidEmail()).
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then()
+                .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+    }
+
 
     @Test
     public void testLoginPasswordWithoutUpperCaseChars() {
@@ -26,6 +37,117 @@ public class ReebokTestApi {
                 .when().post(url)
                 .then()
                 .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+    }
+
+    @Test
+    public void testLoginPasswordWithoutLowerCaseChars() {
+        given().log().all().body(RandomUserData.getBodyPasswordWithoutLowerCaseChars()).
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then()
+                .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+    }
+
+    @Test
+    public void testLoginPasswordWithoutDigits() {
+        given().log().all().body(RandomUserData.getBodyPasswordWithoutDigits()).
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then()
+                .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+    }
+
+    @Test
+    public void testLoginPasswordWithoutSplChars() {
+        given().log().all().body(RandomUserData.getBodyPasswordWithoutSplChars()).
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then()
+                .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+    }
+
+    @Test
+    public void testLoginInvalidLengthPassword() {
+        given().log().all().body(RandomUserData.getBodyInvalidLengthPassword()).
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then().log().all()
+                .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+    }
+
+    @Test
+    public void testLoginWithoutValues() {
+        String email = "";
+        String password = "";
+        given().log().all().body("{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
+                        " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
+                        " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
+                        "code\\n    message\\n    legacyUser\\n    resetPasswordTrigger\\n    __typename\\n  }\\n}\",\"variables\"" +
+                        ":{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}}").
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then().log().all()
+                .statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+    }
+
+    @Test
+    public void testLoginEmailIsNull() {
+
+        String password = "";
+        given().log().all().body("{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
+                        " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
+                        " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
+                        "code\\n    message\\n    legacyUser\\n    resetPasswordTrigger\\n    __typename\\n  }\\n}\",\"variables\"" +
+                        ":{\"email\":\"" + null + "\",\"password\":\"" + password + "\"}}").
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then().log().all()
+                .statusCode(200).body("errors.message[0]", equalTo("Variable \\\"$email\\\" of non-null type \\\"String!\\\" must not be null."));
+    }
+
+    @Test
+    public void testLoginEmailIsInt() {
+        int email = 123;
+        String password = "";
+        given().log().all().body("{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
+                        " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
+                        " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
+                        "code\\n    message\\n    legacyUser\\n    resetPasswordTrigger\\n    __typename\\n  }\\n}\",\"variables\"" +
+                        ":{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}}").
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then().log().all()
+                .statusCode(200).body("errors.message[0]", equalTo("\"Variable \\\"$email\\\" got invalid value 123; Expected type String. String cannot represent a non string value: 123"));
+    }
+
+    @Test
+    public void testLoginUpperCaseInEmailKey() {
+        String email = "";
+        String password = "";
+        given().log().all().body("{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
+                        " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
+                        " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
+                        "code\\n    message\\n    legacyUser\\n    resetPasswordTrigger\\n    __typename\\n  }\\n}\",\"variables\"" +
+                        ":{\"Email\":\"" + email + "\",\"password\":\"" + password + "\"}}").
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then().log().all()
+                .statusCode(200).body("errors.message[0]", equalTo("Variable \"$email\" of required type \"String!\" was not provided."));
+    }
+
+    @Test
+    public void testLoginUpperCaseInPasswordKey() {
+        String email = "";
+        String password = "";
+        given().log().all().body("{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
+                        " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
+                        " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
+                        "code\\n    message\\n    legacyUser\\n    resetPasswordTrigger\\n    __typename\\n  }\\n}\",\"variables\"" +
+                        ":{\"email\":\"" + email + "\",\"Password\":\"" + password + "\"}}").
+                header("Content-Type", "application/json")
+                .when().post(url)
+                .then().log().all()
+                .statusCode(200).body("errors.message[0]", equalTo("Variable \"$password\" of required type \"String!\" was not provided."));
     }
 
 }
