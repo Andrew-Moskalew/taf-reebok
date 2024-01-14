0c27bf14c33a25d2dd663213bf41fad3d7d89d5c
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-14 13:07:55 +0300

    rename class

--- src/test/java/by/itacademy/moskalew/api/ReebokApiTest.java
+++ src/test/java/by/itacademy/moskalew/api/ReebokApiTest.java
@@ -8,7 +8,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 
-public class ReebokTestApi {
+public class ReebokApiTest {
     String url = "https://www.reebok.com/graphql";
 
     @Test
