c5a2da2415f033c4f7b1df0ba83d516c8d6bb4cf
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-18 21:18:00 +0300

    refactoring getBody for int email and password

--- src/main/java/by/itacademy/moskalew/domain/BodyGenerator.java
+++ src/main/java/by/itacademy/moskalew/domain/BodyGenerator.java
@@ -3,7 +3,7 @@ package by.itacademy.moskalew.domain;
 public class BodyGenerator {
 
     public static String getBody(String email, String password) {
-        if (email == null){
+        if (email == null) {
             return "{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
                     " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
                     " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
@@ -24,24 +24,27 @@ public class BodyGenerator {
     }
 
     public static String getBody(int email, String password) {
+        String strEmail = Integer.toString(email);
         return String.format("{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
                 " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
                 " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
                 "code\\n    message\\n    legacyUser\\n    resetPasswordTrigger\\n    __typename\\n  }\\n}\",\"variables\"" +
-                ":{\"email\": %d ,\"password\":\"" + password + "\"}}", email);
+                ":{\"email\": %s ,\"password\":\"" + password + "\"}}", strEmail);
     }
 
     public static String getBody(String email, int password) {
+        String strPassword = Integer.toString(password);
         return String.format("{\"query\":\"mutation userAuthenticate($email: String!, $password: String!, $keepMeLoggedIn: Boolean)" +
                 " {\\n  userAuthenticate(\\n    email: $email\\n    password: $password\\n    keepMeLoggedIn: $keepMeLoggedIn\\n  )" +
                 " {\\n    id\\n    email\\n    name {\\n      firstname\\n      lastname\\n      __typename\\n    }\\n    keepUserLoggedIn\\n    " +
                 "code\\n    message\\n    legacyUser\\n    resetPasswordTrigger\\n    __typename\\n  }\\n}\",\"variables\"" +
-                ":{\"email\":\"" + email + "\",\"password\": %d }}",password);
+                ":{\"email\":\"" + email + "\",\"password\": %s }}", strPassword);
     }
 
     public static String getExpectedResultEmail(int email) {
         return String.format("Variable \"$email\" got invalid value %1$d; Expected type String. String cannot represent a non string value: %1$d", email);
     }
+
     public static String getExpectedResultPassword(int password) {
         return String.format("Variable \"$password\" got invalid value %1$d; Expected type String. String cannot represent a non string value: %1$d", password);
     }
