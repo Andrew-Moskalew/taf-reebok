29721e99eb42cae469897f058a47927958b6686e
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-18 23:13:11 +0300

    add test methods for cart page

---
+++ src/test/java/by/itacademy/moskalew/ui/ReebokCartTest.java
@@ -0,0 +1,43 @@
+package by.itacademy.moskalew.ui;
+
+import by.itacademy.moskalew.driver.MyDriver;
+import by.itacademy.moskalew.pages.ReebokCartPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ReebokCartTest {
+    ReebokCartPage reebokCartPage;
+
+    @BeforeEach
+    public void testStart() {
+        reebokCartPage = new ReebokCartPage();
+        MyDriver.getDriver().get(reebokCartPage.getUrl());
+    }
+
+    @AfterEach
+    public void testFinish() {
+        MyDriver.quitDriver();
+    }
+
+    @Test
+    public void testReebokEmptyCart() {
+        reebokCartPage.clickButtonCart();
+        Assertions.assertEquals("YOUR CART IS EMPTY", reebokCartPage.getTextHeaderEmptyCart());
+    }
+
+    @Test
+    public void testGetItemToCart() {
+        reebokCartPage.addItemToCart();
+        Assertions.assertEquals("Nano X4 Women's Training Shoes",reebokCartPage.getTextMessageShoesInCart());
+    }
+
+    @Test
+    public void testRemoveItemFromCart() {
+        reebokCartPage.addItemToCart();
+        reebokCartPage.clickButtonRemoveFromCart();
+        Assertions.assertEquals("YOUR CART IS EMPTY", reebokCartPage.getTextHeaderEmptyCart());
+    }
+
+}
