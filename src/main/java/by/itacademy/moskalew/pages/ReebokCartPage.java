c8e9fa74493e8b83fcdb0edafbfe4c5200da4fee
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-19 21:44:02 +0300

    refactoring

--- src/main/java/by/itacademy/moskalew/pages/ReebokCartPage.java
+++ src/main/java/by/itacademy/moskalew/pages/ReebokCartPage.java
@@ -67,11 +67,11 @@ public class ReebokCartPage {
 
     public void clickLinkShoes() {
         driver.findElement(By.xpath(linkShoes)).click();
-        new WebDriverWait(MyDriver.getDriver(), Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(getSelectorShoesSizes()));
-    }
+            }
 
     public void clickButtonSize() {
+        new WebDriverWait(MyDriver.getDriver(), Duration.ofSeconds(10))
+                .until(ExpectedConditions.presenceOfElementLocated(getSelectorShoesSizes()));
         driver.findElement(By.xpath(buttonSize)).click();
         //new WebDriverWait(MyDriver.getDriver(), Duration.ofSeconds(10))
                 //.until(ExpectedConditions.presenceOfElementLocated(getButtonAddToCart()));
