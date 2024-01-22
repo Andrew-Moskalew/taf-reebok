194f9bca95d491e615db73c87bf2d6ab8a5b3509
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-23 00:00:11 +0300

    refactoring

--- src/main/java/by/itacademy/moskalew/driver/MyDriver.java
+++ src/main/java/by/itacademy/moskalew/driver/MyDriver.java
@@ -5,13 +5,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyDriver {
     private static WebDriver driver;
+
     public static WebDriver getDriver() {
         if (driver == null) {
+            /*ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            driver = new ChromeDriver(chromeOptions);*/
             driver = new ChromeDriver();
             driver.manage().window().maximize();
         }
         return driver;
     }
+
     public static void quitDriver() {
         if (driver != null) {
             driver.quit();
