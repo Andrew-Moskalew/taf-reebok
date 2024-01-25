11506c45d015ee1213547d765fad4e8fa966e66c
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-26 00:02:42 +0300

    add body generator for test search API

--- src/main/java/by/itacademy/moskalew/domain/BodyGenerator.java
+++ src/main/java/by/itacademy/moskalew/domain/BodyGenerator.java
@@ -83,4 +83,26 @@ public class BodyGenerator {
                 ":{\"email\":\"" + RandomUserData.getRandomValidEmail() + "\",\"PASSWORD\":\"" + RandomUserData.getRandomValidPassword() + "\"}}";
     }
 
+    public static String getBodySearch(String searchRequest) {
+        return String.format("{\"query\":\"query productSearch($category: String, $productType: String, $offset: Int, $limit: Int, $fetchType: String," +
+                " $keyword: String, $facets: [String], $sortBy: String, $sortOrder: String, $locationCode: String, $bruid: String, $deviceType: String," +
+                " $offerDate: String) {\\n  productSearch(\\n    category: $category\\n    productType: $productType\\n    offset: $offset\\n" +
+                "    limit: $limit\\n    fetchType: $fetchType\\n    keyword: $keyword\\n    facets: $facets\\n    sortBy: $sortBy\\n    " +
+                "sortOrder: $sortOrder\\n    locationCode: $locationCode\\n    bruid: $bruid\\n    deviceType: $deviceType\\n    offerDate: $offerDate\\n  ) " +
+                "{\\n    query {\\n      keyword\\n      productType\\n      offset\\n      limit\\n      fetchType\\n      categoryName\\n" +
+                "      categoryPageDetails {\\n        categoryTitle\\n        crumbId\\n        crumbFullId\\n        crumbData\\n        " +
+                "crumbSlug\\n        robots\\n        __typename\\n      }\\n      total\\n      locationCode\\n      bruid\\n      deviceType\\n" +
+                "      __typename\\n    }\\n    results {\\n      ...SearchResultProductFragment\\n      __typename\\n    }\\n    filters {\\n " +
+                "     id\\n      label\\n      facets {\\n        id\\n        label\\n        count\\n        slug\\n        __typename\\n      " +
+                "}\\n      __typename\\n    }\\n    sliderPrice {\\n      min\\n      max\\n      __typename\\n    }\\n    seoCategory\\n    " +
+                "keywordRedirect\\n    redirectCategory\\n    __typename\\n  }\\n}\\n\\nfragment SearchResultProductFragment on SearchResultProduct " +
+                "{\\n  productDiscountPercentage\\n  salePrice\\n  reqularPrice\\n  newProductName\\n  categoryMarketingLine\\n  modelName\\n  " +
+                "productGroup\\n  productId\\n  thumbImage\\n  color\\n  colorGroup\\n  altImage\\n  priceType\\n  division\\n  sport\\n  " +
+                "productType\\n  productLine\\n  productFamily\\n  marketPlaceList\\n  rating\\n  reviewCount\\n  brand\\n  brandAsset\\n  " +
+                "gender\\n  slug\\n  variants {\\n    discountPercentage\\n    variantTitle\\n    regularPrice\\n    salePrice\\n    variantSlug\\n    " +
+                "memberPrice\\n    skuid\\n    variant_thumb_image\\n    badge_text\\n    memberOnly\\n    memberEarly\\n    variant_color_group\\n   " +
+                " __typename\\n  }\\n  __typename\\n}\",\"variables\":{\"category\":null,\"keyword\": \" %s \"," +
+                "\"facets\":[],\"offset\":0,\"limit\":49,\"fetchType\":\"bloomreach\",\"sortBy\":null,\"sortOrder\":null,\"locationCode\":\"VI\"," +
+                "\"bruid\":\"uid=3979741989024:v=15.0:ts=1706212428942:hc=12\",\"deviceType\":\"desktop\"}}", searchRequest);
+    }
 }
