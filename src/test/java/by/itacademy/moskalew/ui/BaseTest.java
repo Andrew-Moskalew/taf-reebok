79d48fd302b265441f0f3f77c29f345094392ea9
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-29 23:55:30 +0300

    add BaseTest class

--- src/test/java/by/itacademy/moskalew/ui/BaseTest.java
+++ src/test/java/by/itacademy/moskalew/ui/BaseTest.java
@@ -1,4 +1,22 @@
 package by.itacademy.moskalew.ui;
 
+import by.itacademy.moskalew.driver.SingletonWebDriver;
+import by.itacademy.moskalew.pages.BasePage;
+import by.itacademy.moskalew.pages.ReebokCartPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class BaseTest {
+    BasePage basePage;
+    @BeforeEach
+    public void baseTestStart() {
+        basePage = new BasePage();
+        SingletonWebDriver.getDriver().get(basePage.getUrl());
+    }
+
+    @AfterEach
+    public void testFinish() {
+        SingletonWebDriver.quitDriver();
+    }
+
 }
