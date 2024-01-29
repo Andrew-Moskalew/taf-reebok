deb41fe5b4595dfdb275bc1486235339f20b3b98
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-01-29 23:32:35 +0300

    refactoring with ReebokApiPage

--- src/test/java/by/itacademy/moskalew/api/ReebokApiTest.java
+++ src/test/java/by/itacademy/moskalew/api/ReebokApiTest.java
@@ -3,6 +3,7 @@ package by.itacademy.moskalew.api;
 
 import by.itacademy.moskalew.domain.BodyGenerator;
 import by.itacademy.moskalew.domain.RandomUserData;
+import by.itacademy.moskalew.pages.ReebokApiPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
@@ -12,16 +13,16 @@ import static org.hamcrest.Matchers.equalTo;
 
 
 public class ReebokApiTest {
-    String url = "https://www.reebok.com/graphql";
+    ReebokApiPage reebokApiPage;
     private static final Logger logger = LogManager.getLogger();
 
     @Test
     public void testLoginValidValues() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), RandomUserData.getRandomValidPassword()))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
@@ -29,9 +30,9 @@ public class ReebokApiTest {
     public void testLoginInvalidEmail() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody(RandomUserData.getRandomInvalidEmail(), RandomUserData.getRandomValidPassword()))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
@@ -40,9 +41,9 @@ public class ReebokApiTest {
     public void testLoginPasswordWithoutUpperCaseChars() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), RandomUserData.getRandomPasswordWithoutUpperCaseChars()))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
@@ -50,9 +51,9 @@ public class ReebokApiTest {
     public void testLoginPasswordWithoutLowerCaseChars() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), RandomUserData.getRandomPasswordWithoutLowerCaseChars()))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
@@ -60,9 +61,9 @@ public class ReebokApiTest {
     public void testLoginPasswordWithoutDigits() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), RandomUserData.getRandomPasswordWithoutDigits()))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
@@ -70,9 +71,9 @@ public class ReebokApiTest {
     public void testLoginPasswordWithoutSplChars() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), RandomUserData.getRandomPasswordWithoutSplChars()))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
@@ -80,9 +81,9 @@ public class ReebokApiTest {
     public void testLoginInvalidLengthPassword() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), RandomUserData.getRandomInvalidLengthPassword()))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
@@ -90,29 +91,29 @@ public class ReebokApiTest {
     public void testLoginWithEmptyValues() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         given().body(BodyGenerator.getBody("", ""))
-                .header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Incorrect email/password – please check and retry"));
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_INCORRET_VALUES));
         logger.info("Test passed \n");
     }
 
     @Test
     public void testLoginEmailIsNull() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
-        given().body(BodyGenerator.getBody(null, RandomUserData.getRandomValidPassword())).
-                header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Variable \"$email\" of non-null type \"String!\" must not be null."));
+        given().body(BodyGenerator.getBody(null, RandomUserData.getRandomValidPassword()))
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_EMAIL_NULL));
         logger.info("Test passed \n");
     }
 
     @Test
     public void testLoginPasswordIsNull() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
-        given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), null)).
-                header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Variable \"$password\" of non-null type \"String!\" must not be null."));
+        given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), null))
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_PASSWORD_NULL));
         logger.info("Test passed \n");
     }
 
@@ -120,10 +121,10 @@ public class ReebokApiTest {
     public void testLoginEmailIsInt() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         int email = RandomUserData.getRandomIntEmail();
-        given().body(BodyGenerator.getBody(email, RandomUserData.getRandomValidPassword())).
-                header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo(BodyGenerator.getExpectedResultIntEmail(email)));
+        given().body(BodyGenerator.getBody(email, RandomUserData.getRandomValidPassword()))
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.getExpectedResultIntEmail(email)));
         logger.info("Test passed \n");
     }
 
@@ -131,42 +132,41 @@ public class ReebokApiTest {
     public void testLoginPasswordIsInt() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
         int password = RandomUserData.getRandomIntPassword();
-        given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), password)).
-                header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo(BodyGenerator.getExpectedResultIntPassword(password)));
+        given().body(BodyGenerator.getBody(RandomUserData.getRandomValidEmail(), password))
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.getExpectedResultIntPassword(password)));
         logger.info("Test passed \n");
     }
 
     @Test
     public void testLoginUpperCaseInEmailKey() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
-        given().body(BodyGenerator.getBodyWithInvalidEmailKey()).
-                header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Variable \"$email\" of required type \"String!\" was not provided."));
+        given().body(BodyGenerator.getBodyWithInvalidEmailKey())
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_EMAIL_KEY_INVALID));
         logger.info("Test passed \n");
     }
 
     @Test
     public void testLoginUpperCaseInPasswordKey() {
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
-        given().body(BodyGenerator.getBodyWithInvalidPasswordKey()).
-                header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("errors.message[0]", equalTo("Variable \"$password\" of required type \"String!\" was not provided."));
+        given().body(BodyGenerator.getBodyWithInvalidPasswordKey())
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("errors.message[0]", equalTo(ReebokApiPage.ERROR_MESSAGE_PASSWORD_KEY_INVALID));
         logger.info("Test passed \n");
     }
 
     @Test
     public void testSearch() {
-        String searchRequest = "Spyder x Reebok Zig Kinetica 2.5 Edge Shoes";
         logger.info("Started " + Thread.currentThread().getStackTrace()[1].getMethodName());
-        logger.info("search request is: " + searchRequest);
-        given().body(BodyGenerator.getBodySearch(searchRequest)).
-                header("Content-Type", "application/json")
-                .when().post(url)
-                .then().statusCode(200).body("data.productSearch.results.newProductName[0]", equalTo(searchRequest));
+        logger.info("search request is: " + ReebokApiPage.SEARCH_REQUEST);
+        given().body(BodyGenerator.getBodySearch(ReebokApiPage.SEARCH_REQUEST))
+                .contentType(ReebokApiPage.CONTENT_TYPE)
+                .when().post(ReebokApiPage.URL)
+                .then().statusCode(200).body("data.productSearch.results.newProductName[0]", equalTo(ReebokApiPage.SEARCH_REQUEST));
         logger.info("Test passed \n");
     }
 
