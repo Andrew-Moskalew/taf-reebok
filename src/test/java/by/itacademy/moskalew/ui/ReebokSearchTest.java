9a563b7268b5aa919eb99dbbbd49938aadcbf777
Andrew-Moskalew <Lorenzo4474@gmail.com>
2024-02-02 19:34:40 +0300

    refactoring

--- src/test/java/by/itacademy/moskalew/ui/ReebokSearchTest.java
+++ src/test/java/by/itacademy/moskalew/ui/ReebokSearchTest.java
@@ -14,7 +14,7 @@ public class ReebokSearchTest extends BaseTest {
 
     private static final String SEARCH_REQUEST = "Nano X4 Training Shoes";
     private static final String SEARCH_RESPONSE_NAME_OF_ITEM = "”NANO X4 TRAINING SHOES”";
-    private static final int SEARCH_RESPONSE_NUMBER_OF_ITEMS = 12;
+    private static final int SEARCH_RESPONSE_NUMBER_OF_ITEMS = 14;
     ReebokSearchPage reebokSearchPage;
     private static final Logger logger = LogManager.getLogger();
 
